47ef155472e86cb748504747c12b34c1a5e081c7
Dmitry Romenskiy (DevExpress) <romenskiy.dmitry@corp.devexpress.com>
2018-09-03 16:41:16 +0300

    add example content

---
+++ CS/LookUpEditFilterExample/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.ObjectModel;
+
+namespace LookUpEditFilterExample.ViewModels {
+    [POCOViewModel]
+    public class MainViewModel {
+        protected MainViewModel() {
+            Data = new ObservableCollection<Data>();
+            Random r = new Random();
+            for (int i = 0; i < 25; i++)
+                Data.Add(new Data() { ID = i, Name = "Name " + i, Value = r.Next(0, 25) });
+        }
+
+        public virtual ObservableCollection<Data> Data { get; set; }
+    }
+
+    public class Data {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+}
