209f7de417a0807fc756efa607057cb1e20f2f69
dxIvan <38530056+ivansupport@users.noreply.github.com>
2018-12-26 13:41:39 +0300

    replace a custom descendant with the built-in functionality

--- CS/LookUpEditFilterExample/MainWindow.xaml.cs
+++ CS/LookUpEditFilterExample/MainWindow.xaml.cs
@@ -1,10 +1,5 @@
-﻿using DevExpress.Xpf.Core;
-
-namespace LookUpEditFilterExample {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : DXWindow {
+﻿namespace LookUpEditFilterExample {
+    public partial class MainWindow {
         public MainWindow() {
             InitializeComponent();
         }
--- CS/LookUpEditFilterExample/Views/MainView.xaml.cs
+++ CS/LookUpEditFilterExample/Views/MainView.xaml.cs
@@ -1,12 +1,18 @@
-﻿using System.Windows.Controls;
+﻿using DevExpress.Data.Filtering;
+using DevExpress.Xpf.Editors.Helpers;
+using System.Windows.Controls;
 
 namespace LookUpEditFilterExample.Views {
-    /// <summary>
-    /// Interaction logic for MainView.xaml
-    /// </summary>
     public partial class MainView : UserControl {
         public MainView() {
             InitializeComponent();
+        }
+        private void SubstituteDisplayFilter(object sender, SubstituteDisplayFilterEventArgs e) {
+            if (string.IsNullOrEmpty(e.DisplayText))
+                return;
+            var extraFilter = CriteriaOperator.Parse("Contains(Value,?)", e.DisplayText);
+            e.DisplayFilter = new GroupOperator(GroupOperatorType.Or, extraFilter, e.DisplayFilter);
+            e.Handled = true;
         }
     }
 }
